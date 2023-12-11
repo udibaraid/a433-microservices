942cb1965bbd490cdbaaf630d011d5b5d659f60d
udibaraid <udibaraid@udibaraid.udibara.id>
2023-12-11 12:43:56 +0700

    Add Comment

--- build_push_image_karsajobs.sh
+++ build_push_image_karsajobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# build Docker image dari berkas Dockerfile
+# build Docker image dari berkas Dockerfile (Kasjajobs)
 docker build -t ghcr.io/udibaraid/karsajobs:latest .
 
 # login ke Github Packages
