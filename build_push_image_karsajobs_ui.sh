5da549b87e4eef2af60503f1fb2152262e8362d1
udibaraid <udibaraid@udibaraid.udibara.id>
2023-12-11 11:41:10 +0700

    Add script and CI config

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#build Docker image dari berkas Dockerfile
+docker build -t ghcr.io/udibaraid/karsajobs-ui:latest .
+
+# login ke Github Packages
+echo $CR_PAT | docker login ghcr.io -u udibaraid --password-stdin
+
+#push image ke Github Packages
+docker push ghcr.io/udibaraid/karsajobs-ui:latest
